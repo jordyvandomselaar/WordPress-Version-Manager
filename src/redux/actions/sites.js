599bb5be474f66c94aba888a370a9f0dbb9fcafb
jordy <jordy.van.domselaar@outlook.com>
2017-12-06 02:21:48 +0100

    Docblock.

--- src/redux/actions/sites.js
+++ src/redux/actions/sites.js
@@ -1,4 +1,11 @@
 import {createAction} from "./base"
 import {ADD_SITE} from "./types/sites"
 
+/**
+ * Add a new site to the store.
+ *
+ * @param site
+ * @returns {{type: *}}
+ * @constructor
+ */
 export const AddSite = site => createAction(ADD_SITE, {site})
