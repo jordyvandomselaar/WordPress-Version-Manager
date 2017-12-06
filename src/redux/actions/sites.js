d8bb29a4cc68caaa198d96751d2eabd15aa4633a
jordy <jordy.van.domselaar@outlook.com>
2017-12-06 02:23:14 +0100

    Wth phpstorm. 's not a constructor.

--- src/redux/actions/sites.js
+++ src/redux/actions/sites.js
@@ -2,10 +2,8 @@ import {createAction} from "./base"
 import {ADD_SITE} from "./types/sites"
 
 /**
- * Add a new site to the store.
  *
  * @param site
  * @returns {{type: *}}
- * @constructor
  */
 export const AddSite = site => createAction(ADD_SITE, {site})
