52e523b8e1a502f2855d143f1bb05ccaa4c7696a
jordy <jordy.van.domselaar@outlook.com>
2017-12-07 06:56:37 +0100

    Refactor folder structure.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./components/App"
+import App from "./app/components/App/index"
 import registerServiceWorker from "./registerServiceWorker"
-import Store from "./redux/store"
+import Store from "./app/redux/store/index"
 
 ReactDOM.render(
     <Store>
